52cab13894a3ccdd41379a9523ee3c28c852bf36
Anatay Smankulov <111154406+AnataySmn@users.noreply.github.com>
2023-07-24 15:47:41 -0400

    Add files via upload

---
+++ quiz-app/src/helpers/Questions.js
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+      prompt: "What is ReactJS??",
+      optionA: "Server-side Framework",
+      optionB: "User-interface framework",
+      optionC: "A Library for building interaction interfaces",
+      optionD: "None of the Above",
+      answer: "optionC",
+    },
+    {
+      prompt: "Which of the following is must for the API in React.js?",
+      optionA: "SetinitialComponent",
+      optionB: "renderComponent",
+      optionC: "render",
+      optionD: "All of the above",
+      answer: "optionC",
+    },
+    {
+      prompt: "Which of this is not a javascript framework?",
+      optionA: "React",
+      optionB: "Angular",
+      optionC: "Vue",
+      optionD: "Java",
+      answer: "optionD",
+    },
+  ];
